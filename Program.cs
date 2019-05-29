d21ec96bc954b342d970b196dba0a946f1770135
Michael Yankura <myankura210@gmail.com>
2019-05-29 08:49:24 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace dictionary_of_words
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+    /*
+    Create a dictionary with key value pairs to
+    represent words (key) and its definition (value)
+    */
+            Dictionary<string, string> wordsAndDefinitions = new Dictionary<string, string>();
+
+            // Add several more words and their definitions
+            wordsAndDefinitions.Add("Awesome", "The feeling of students when they are learning C#");
+
+            /*
+                Use square bracket lookup to get the definition two
+                words and output them to the console
+            */
+
+            /*
+                Loop over dictionary to get the following output:
+                    The definition of [WORD] is [DEFINITION]
+                    The definition of [WORD] is [DEFINITION]
+                    The definition of [WORD] is [DEFINITION]
+            */
+            foreach (KeyValuePair<string, string> word in wordsAndDefinitions)
+            {
+            }
+        }
+    }
+}
